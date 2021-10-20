0fc0be11bd9743ab1ddf87754ef3a77546698484
drumman22 <grossmanjason0@gmail.com>
2021-10-20 19:10:18 -0400

    Added fxml

--- src/main/java/baseline/Application.java
+++ src/main/java/baseline/Application.java
@@ -1,19 +1,18 @@
 package baseline;
 
-import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
