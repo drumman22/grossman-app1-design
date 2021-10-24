3ef2c7eccc6b3f2c70892df6cf60cd604f3b8227
drumman22 <grossmanjason0@gmail.com>
2021-10-24 18:49:42 -0400

    TodoList

---
+++ src/main/java/baseline/TodoList.java
@@ -0,0 +1,66 @@
+/*
+ *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Jason Grossman
+ */
+
+package baseline;
+
+import java.util.List;
+
+public class TodoList {
+    private String title;
+    private List<Item> itemList;
+
+    public TodoList(String title) {
+        // call other constructor with an empty list
+    }
+
+    public TodoList(String title, List<Item> itemList) {
+        // set values
+    }
+
+    public void addItem(Item item) {
+        // add item into itemList
+    }
+
+    public void updateItem(Item prevItem, Item newItem) {
+        // update item by replacing the previous with the new
+    }
+
+    public void removeItem(Item item) {
+        // remove item
+    }
+
+    public List<Item> getItemList() {
+        // return itemList
+        return null; // temp return
+    }
+
+    public List<Item> getComplete() {
+        // create a new list
+        // iterate through itemList
+        // add items that are only completed (isComplete == true) to new list
+        return null; // temp return
+    }
+
+    public List<Item> getIncomplete() {
+        // create a new list
+        // iterate through itemList
+        // add items that are incomplete (isComplete == false) to new list
+        return null; // temp return
+    }
+
+    public List<Item> getSortedByDate() {
+        // sort list by due date
+        return null; // temp return
+    }
+
+    public void setTitle(String title) {
+        // set new title
+    }
+
+    public String getTitle() {
+        // return title
+        return ""; // temp return
+    }
+}
---
+++ src/test/java/baseline/TodoListTest.java
@@ -0,0 +1,49 @@
+/*
+ *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Jason Grossman
+ */
+
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoListTest {
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void updateItem() {
+    }
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void getItemList() {
+    }
+
+    @Test
+    void getComplete() {
+    }
+
+    @Test
+    void getIncomplete() {
+    }
+
+    @Test
+    void getSortedByDate() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+}
